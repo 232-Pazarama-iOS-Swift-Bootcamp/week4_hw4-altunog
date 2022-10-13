b5a2cdfed4b0d1f1b3ad9a1c44ef70ab5bda90a4
altunog <altunog@gmail.com>
2022-10-13 23:06:25 +0300

    firebase, snapkit, moya, iqkeyboarmanager packages are installed. scenedelegate removed. window has been set up in appdelegate.

---
+++ FlickrApp/FlickrApp/AuthVC.swift
@@ -0,0 +1,20 @@
+//
+//  AuthVC.swift
+//  FlickrApp
+//
+//  Created by Oğuz Kaan Altun on 13.10.2022.
+//
+
+import UIKit
+
+class AuthVC: UIViewController {
+
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+
+}
